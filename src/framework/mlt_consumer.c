d08db62fc23db46ce253400894dc2ede6b613518
gabrieljablonski <gabriel.g.jablonski@gmail.com>
2021-09-16 11:13:53 -0300

    dynamic target loudness

--- src/framework/mlt_consumer.c
+++ src/framework/mlt_consumer.c
@@ -1635,11 +1635,12 @@ mlt_frame mlt_consumer_rt_frame( mlt_consumer self )
 			mlt_properties_set_data( MLT_FRAME_PROPERTIES( frame ), "consumer", self, 0, NULL, NULL );
 		}
 	}
+
 	if (!audio_off && mlt_properties_get(properties, "target_loudness")) {
 		if (!priv->loudness) {
 			priv->loudness = mlt_factory_filter( NULL, "dynamic_loudness", NULL );
-			mlt_properties_set_double(MLT_FILTER_PROPERTIES(priv->loudness), "target_loudness", mlt_properties_get_double(properties, "target_loudness"));
 		}
+		mlt_properties_set_double(MLT_FILTER_PROPERTIES(priv->loudness), "target_loudness", mlt_properties_get_double(properties, "target_loudness"));
 		if (priv->loudness) {
 			priv->loudness->process(priv->loudness, frame);
 		}
